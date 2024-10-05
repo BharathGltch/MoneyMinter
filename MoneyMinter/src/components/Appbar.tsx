516cb9e7ec0b1ad1e3e6b3eae3878919187a1646
Bharath Chirumamilla <bharath.chirumamilla@zelis.com>
2024-10-05 11:45:14 +0530

    Added HAmburger menu

--- MoneyMinter/src/components/Appbar.tsx
+++ MoneyMinter/src/components/Appbar.tsx
@@ -3,14 +3,14 @@ import { MyContext } from "./ContextProvider"
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaGithub } from "react-icons/fa";
-import { CiMenuBurger } from "react-icons/ci";
 import MenuIcon from '@mui/icons-material/Menu';
+
 
 
 
 export default function Appbar() {
 
-    const [mobileMenuOpen,setMobileMenuOpen]=useState<boolean>();
+    const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
     const context = useContext(MyContext);
     console.log("Context inside appbar is ", context);
     const navigate = useNavigate();
@@ -28,6 +28,10 @@ export default function Appbar() {
             newWindow.opener = null;
     }
 
+    const handleMenuClick=()=>{
+        setMobileMenuOpen((prevVal)=>!prevVal);
+    }
+
 
 
     if (context?.isLoggedIn === true) {
@@ -38,7 +42,7 @@ export default function Appbar() {
                         <Link to="/" className="mt-4 ml-9">
                             <img src={"https://lirp.cdn-website.com/eb1755b3/dms3rep/multi/opt/OWAY-1314w.png"} width={100} height={100} />
                         </Link>
-                        
+
                     </div>
 
                     <div className="mt-4 mb-1 mr-6 flex">
@@ -61,30 +65,39 @@ export default function Appbar() {
 
     const handleRegister = () => {
         navigate("/register");
-    }   
+    }
     return (
-        <div className="container mx-auto px-1">
+        <div className="container mx-auto px-4">
             <div className="flex justify-between items-center py-4">
                 <div className="flex items-center">
                     <Link to="/" className="flex items-center px-2">
-                        <img src={"https://lirp.cdn-website.com/eb1755b3/dms3rep/multi/opt/OWAY-1314w.png"} alt="Logo" className="max-h-8 sm:max-h-12  w-auto h-auto" />
+                        <img src={"https://lirp.cdn-website.com/eb1755b3/dms3rep/multi/opt/OWAY-1314w.png"} alt="Logo" className="max-h-8 sm:max-h-10  w-auto h-auto" />
                     </Link>
-                   
+
                 </div>
 
                 <div className="hidden sm:flex items-center space-x-4">
-                        <Button onClick={handleContribute} variant="contained" ><FaGithub className="mr-2" />Contribute</Button>                
-                   
-                        <Button onClick={handleRegister} variant="contained" >SignUp</Button>
-                    
-                        <Button onClick={handleLogin} variant="contained" >Login</Button>
+                    <Button onClick={handleContribute} variant="contained" ><FaGithub className="mr-2" />Contribute</Button>
+
+                    <Button onClick={handleRegister} variant="contained" >SignUp</Button>
+
+                    <Button onClick={handleLogin} variant="contained" >Login</Button>
                 </div>
 
                 <div className="sm:hidden">
-                       <Button>
-                        <MenuIcon/>
-                        </Button>
-                       
+                    <Button onClick={handleMenuClick}>
+                        <MenuIcon />
+                    </Button>
+                    {mobileMenuOpen &&
+                     <div className="sm:hidden fixed inset-x-0 top-16 bg-gray-900 shadow-lg z-50 transition-all duration-300 ease-in-out">
+                                <div className="flex flex-col items-center py-4 space-y-2">
+                                           
+                        <Button onClick={handleContribute} variant="contained" fullWidth><FaGithub className="mr-2" />Contribute</Button>
+                        <Button onClick={handleRegister} variant="contained" fullWidth>SignUp</Button>
+                        <Button onClick={handleLogin} variant="contained" fullWidth>Login</Button>             
+                                </div>
+                        </div>}
+
                 </div>
             </div>
         </div>
