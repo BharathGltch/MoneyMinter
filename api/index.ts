0082e2f1d775fae8becc6c9dcc800749270f9d26
bharathch <bharrathch@gmail.com>
2024-10-08 23:30:58 +0530

    Changed CORS Policy

--- api/index.ts
+++ api/index.ts
@@ -31,7 +31,7 @@ const corsOptions = {
 };
 
 const allowCrossDomain = (req:Request, res:Response, next:NextFunction) => {
-  res.header(`Access-Control-Allow-Origin`, `example.com`);
+  res.header(`Access-Control-Allow-Origin`, `http://localhost:5173`);
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
   next();
